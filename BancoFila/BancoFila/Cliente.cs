8da573f595146ee8e62f41a36ffa7f859eb69e16
gabis <gabriella.maris@etec.sp.gov.br>
2024-05-15 17:58:35 -0300

    adc banco

---
+++ BancoFila/BancoFila/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoFila
+{
+    class Cliente
+    {
+        public string Nome { get {return this.Nome;}}
+        public int Idade { get { return this.Idade;}}
+        public string Prioritário{ get { return this.Prioritário; }}
+
+
+
+        public void MostrarDados(string nome, int Idade, string Prioritário)
+        {
+            Console.WriteLine(nome);
+            Console.WriteLine(Idade);
+            Console.WriteLine(Prioritário);
+        }
+
+    }
+}
---
+++ BancoFila/BancoFila/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoFila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Nome = new string[10];
+            int[] Idade = new int[10];
+            string[] Prioritário = new string[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("1- Cadastrar o cliente.\n" +
+                    "2- Inserir o cliente na lista.\n" +
+                    "3- Listar a fila de clientes atuais.\n" +
+                    "4- Incluir clientes prioritários.\n" +
+                    "5- Digite q para sair do programa.");
+                Console.ReadLine();
+
+                Console.WriteLine("Digite o seu nome:");
+                Console.ReadLine();
+                Console.WriteLine("Digite a sua idade:");
+                int.Parse(Console.ReadLine());
+                Console.WriteLine("Você é um cliente prioritário? Sim/Não");
+                Console.ReadLine();
+
+                for (int p = 0; p < Nome.Length; p++)
+                {
+                    Console.WriteLine("Nome: ");
+                    Nome[p] = Console.ReadLine();
+
+                    Console.WriteLine("Idade: ");
+                    Idade[p] = Convert.ToInt16(Console.ReadLine());
+
+
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+ }
+
